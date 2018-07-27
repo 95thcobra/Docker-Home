e3a98cdcf66e7de5f18943e94e428e033f2143ff
Marwolf <cleako@gmail.com>
2018-07-27 12:22:08 -0400

    HTTPS with Dockerized Lets Encrypt certbot

---
+++ certbot.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+docker run -it --rm --name certbot \
+        -v `pwd`/etc/certbot/certs:/etc/letsencrypt \
+        -v `pwd`/etc/certbot/data:/data/letsencrypt \
+        deliverous/certbot \
+        certonly \
+        --webroot --webroot-path=/data/letsencrypt \
+        -d openrsc.com -d www.openrsc.com
