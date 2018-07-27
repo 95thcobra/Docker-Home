e94dcfd9470907d10def072a71cd518e6da3cb2e
Marwolf <cleako@gmail.com>
2018-07-27 12:40:37 -0400

    Path correction to letsencrypt

--- certbot.sh
+++ certbot.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 sudo docker run -it --rm --name certbot \
-        -v `pwd`/etc/certbot/certs:/etc/letsencrypt \
-        -v `pwd`/etc/certbot/data:/data/letsencrypt \
+        -v `pwd`/etc/letsencrypt/certs:/etc/letsencrypt \
+        -v `pwd`/etc/letsencrypt/data:/data/letsencrypt \
         deliverous/certbot \
         certonly \
         --webroot --webroot-path=/data/letsencrypt \
