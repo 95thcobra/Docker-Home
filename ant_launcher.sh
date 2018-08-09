84f1634569e75f0adc6897d6100ed2503a226bd6
Marwolf <cleako@gmail.com>
2018-08-09 14:31:13 -0400

    Server console outputs to gameserver.log

--- ant_launcher.sh
+++ ant_launcher.sh
@@ -5,5 +5,6 @@ echo ""
 echo "Running the game server. Press CTRL + C to shut it down or"
 echo "CTRL + A + D to detached the screen so this runs in the background."
 echo ""
+echo "Console output is being saved to gameserver.log"
 echo ""
-ant -f Game/server/build.xml runserver
+ant -f Game/server/build.xml runserver > gameserver.log
