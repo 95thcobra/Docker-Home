2fb24ef547e65fb15a8af483054bbdc0dfb0e925
Marwolf <cleako@gmail.com>
2018-08-09 14:45:21 -0400

    Corrected verbiage

--- ant_launcher.sh
+++ ant_launcher.sh
@@ -3,7 +3,7 @@
 # Run the game server
 echo ""
 echo "Running the game server. Press CTRL + C to shut it down or"
-echo "CTRL + A + D to detached the screen so this runs in the background."
+echo "CTRL + A + D to detach the screen so this continues in the background."
 echo ""
 echo "Console output is being saved to gameserver.log"
 echo ""
