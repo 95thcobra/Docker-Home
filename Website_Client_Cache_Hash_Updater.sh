76948f3b7af62629b9ea6a211cc114df99744416
Marwolf <cleako@gmail.com>
2018-08-11 00:16:34 -0400

    Forced sudo for actions due to default permissions

--- Website_Client_Cache_Hash_Updater.sh
+++ Website_Client_Cache_Hash_Updater.sh
@@ -3,20 +3,20 @@
 # Client
 echo "Compiling the game client."
 echo ""
-ant -f "Game/client/build.xml" compile
+sudo ant -f "Game/client/build.xml" compile
 echo ""
 echo ""
 echo "Compressing jar into zip"
 echo ""
 cd Game/client
-zip -r "client.zip" "Open_RSC_Client.jar"
+sudo zip -r "client.zip" "Open_RSC_Client.jar"
 cd ../../
 echo ""
 echo ""
 echo "Copying result to Website/downloads/"
 echo ""
-yes | cp -rf "Game/client/client.zip" "Website/downloads/"
-rm "Game/client/client.zip"
+yes | sudo cp -rf "Game/client/client.zip" "Website/downloads/"
+sudo rm "Game/client/client.zip"
 
 echo ""
 echo ""
@@ -24,20 +24,20 @@ echo ""
 # Launcher
 echo "Compiling the game launcher."
 echo ""
-ant -f "Game/Launcher/build.xml"
+sudo ant -f "Game/Launcher/build.xml"
 echo ""
 echo ""
 echo "Copying result to Website/downloads/"
 echo ""
-yes | cp -rf "Game/Launcher/dist/Open_RSC_Launcher.jar" "Website/downloads/"
+yes | sudo cp -rf "Game/Launcher/dist/Open_RSC_Launcher.jar" "Website/downloads/"
 echo ""
 echo ""
 
 # Cache
 echo "Copying Game/client/cache.zip to Website/downloads/"
-yes | cp -rf "Game/client/cache.zip" "Website/downloads/"
+yes | sudo cp -rf "Game/client/cache.zip" "Website/downloads/"
 
 # MD5sum client.zip and cache.zip > hashes.txt
-rm Website/downloads/hashes.txt
-md5sum Website/downloads/client.zip | grep ^[a-zA-Z0-9]* | awk '{print "client="$1}' | tee -a Website/downloads/hashes.txt
-md5sum Website/downloads/cache.zip | grep ^[a-zA-Z0-9]* | awk '{print "cache="$1}' | tee -a Website/downloads/hashes.txt
+sudo rm Website/downloads/hashes.txt
+md5sum Website/downloads/client.zip | grep ^[a-zA-Z0-9]* | awk '{print "client="$1}' | sudo tee -a Website/downloads/hashes.txt
+md5sum Website/downloads/cache.zip | grep ^[a-zA-Z0-9]* | awk '{print "cache="$1}' | sudo tee -a Website/downloads/hashes.txt
