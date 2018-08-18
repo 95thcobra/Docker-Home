4e7f7674983ccc2a5dab37588ca4cb2c2d63574f
Marwolf <cleako@gmail.com>
2018-08-18 16:15:42 -0400

    Further changes

--- Lets_Encrypt.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo docker run -it --rm --name certbot \
-        -v `pwd`/etc/letsencrypt/certs:/opt/bitnami/nginx/conf/bitnami/certs \
-        deliverous/certbot \
-        certonly \
-        --webroot --webroot-path=/data/letsencrypt \
-        -d localhost -d www.localhost --staging
