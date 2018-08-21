47fe76d70e666765adf68e1d34304eca837b7ee4
Marwolf <cleako@gmail.com>
2018-08-21 07:04:22 -0400

    Removal of unsupported OSes and versions, simplification of script

--- Linux_Installer.sh
+++ Linux_Installer.sh
@@ -44,27 +44,15 @@ if [ "$install" == "1" ]; then
         echo ""
         echo "Which Ubuntu Linux version are you running?"
         echo ""
-        echo "${RED}1${NC} - Xenial 16.04"
-        echo "${RED}2${NC} - Yakkety 16.10"
-        echo "${RED}3${NC} - Zesty 17.04"
-        echo "${RED}4${NC} - Artful 17.10"
-        echo "${RED}5${NC} - Bionic 18.04"
-        echo "${RED}6${NC} - Cosmic 18.10"
+        echo "${RED}1${NC} - Bionic 18.04"
+        echo "${RED}2${NC} - Cosmic 18.10"
         echo ""
         read ubuntu
 
         # Ubuntu Version ===================================================>
         if [ "$ubuntu" == "1" ]; then
-            vers="xenial"
-        elif [ "$ubuntu" == "2" ]; then
-            vers="yakkety"
-        elif [ "$ubuntu" == "3" ]; then
-            vers="zesty"
-        elif [ "$ubuntu" == "4" ]; then
-            vers="artful"
-        elif [ "$ubuntu" == "5" ]; then
             vers="bionic"
-        elif [ "$ubuntu" == "6" ]; then
+        elif [ "$ubuntu" == "2" ]; then
             vers="cosmic"
         else
             vers="bionic"
@@ -143,197 +131,6 @@ if [ "$install" == "1" ]; then
         fi
         # Ubuntu Java <===================================================
     # Ubuntu OS <===================================================
-
-    # Fedora OS ===================================================>
-    elif [ "$os" == "2" ]; then
-        echo ""
-        echo ""
-        echo "Verifying the basics are installed."
-        echo ""
-        sudo dnf -y update && sudo dnf -y upgrade && sudo dnf -y install zip fail2ban screen make unzip git ca-certificates curl yum-utils device-mapper-persistent-data lvm2
-        sudo systemctl start fail2ban
-        sudo systemctl enable fail2ban
-        echo ""
-        echo ""
-        echo "Permitting good ports through the firewall."
-        echo ""
-        firewall-cmd --permanent --add-port=53595/tcp
-        firewall-cmd --permanent --add-port=8080/tcp
-        firewall-cmd --permanent --add-port=9000/tcp
-        firewall-cmd --permanent --add-port=80/tcp
-        firewall-cmd --permanent --add-port=443/tcp
-        firewall-cmd --permanent --add-port=22/tcp
-        firewall-cmd --reload
-        echo ""
-        echo ""
-        echo "Do you have Oracle Java JDK 8 and Ant installed already? It is required for this."
-        echo ""
-        echo "${RED}1${NC} - Install for me!"
-        echo "${RED}2${NC} - Im all set"
-        echo ""
-        echo "Which of the above do you wish to do? Type the choice number and press enter."
-        echo ""
-        echo ""
-        read java
-
-        # Fedora Java ===================================================>
-        if [ "$java" == "1" ]; then
-            sudo dnf -y install ant openjfx
-            wget --no-cookies \
-            --no-check-certificate \
-            --header "Cookie: oraclelicense=accept-securebackup-cookie" \
-            "http://download.oracle.com/otn-pub/java/jdk/8-b132/jdk-8-linux-x64.rpm" \
-            -O jdk-8-linux-x64.rpm
-            sudo rpm -Uvh jdk-8-linux-x64.rpm
-            sudo alternatives --install /usr/bin/java java /usr/java/jdk1.8.0/jre/bin/java 20000
-            sudo alternatives --install /usr/bin/jar jar /usr/java/jdk1.8.0/bin/jar 20000
-            sudo alternatives --install /usr/bin/javac javac /usr/java/jdk1.8.0/bin/javac 20000
-            sudo alternatives --install /usr/bin/javaws javaws /usr/java/jdk1.8.0/jre/bin/javaws 20000
-        else
-          continue
-        fi
-        # Fedora Java <===================================================
-
-        echo ""
-        echo ""
-        echo "Do you have Docker installed? It is required for this."
-        echo ""
-        echo "${RED}1${NC} - No, install it for me!"
-        echo "${RED}2${NC} - Yes"
-        echo ""
-        echo "Which of the above do you wish to do? Type the choice number and press enter."
-        echo ""
-        echo ""
-        read docker
-
-        # Fedora Docker ===================================================>
-        if [ "$docker" == "1" ]; then
-            echo "Removing any old versions of Docker that might confict."
-            echo ""
-            sudo dnf -y remove docker \
-                 docker-client \
-                 docker-client-latest \
-                 docker-common \
-                 docker-latest \
-                 docker-latest-logrotate \
-                 docker-logrotate \
-                 docker-selinux \
-                 docker-engine-selinux \
-                 docker-engine
-            echo ""
-            echo ""
-            echo "Attempting to install Docker now."
-            echo ""
-            sudo dnf -y install dnf-plugins-core
-            sudo dnf -y config-manager \
-              --add-repo \
-              https://download.docker.com/linux/fedora/docker-ce.repo
-            sudo dnf -y install docker-ce docker-compose
-            sudo systemctl start docker
-            sudo systemctl enable docker
-            sudo usermod -aG docker $USER
-        else
-          continue
-        fi
-        # Fedora Docker <===================================================
-    # Fedora OS <===================================================
-
-    # CentOS 7 OS ===================================================>
-    elif [ "$os" == "3" ]; then
-        echo ""
-        echo ""
-        echo "Verifying the basics are installed."
-        echo ""
-        sudo wget http://springdale.math.ias.edu/data/puias/unsupported/7/x86_64//dnf-0.6.4-2.sdl7.noarch.rpm
-        sudo wget http://springdale.math.ias.edu/data/puias/unsupported/7/x86_64/dnf-conf-0.6.4-2.sdl7.noarch.rpm
-        sudo wget http://springdale.math.ias.edu/data/puias/unsupported/7/x86_64/python-dnf-0.6.4-2.sdl7.noarch.rpm
-        sudo yum -y install dnf-0.6.4-2.sdl7.noarch.rpm dnf-conf-0.6.4-2.sdl7.noarch.rpm python-dnf-0.6.4-2.sdl7.noarch.rpm
-        sudo dnf -y update && sudo dnf -y upgrade && sudo dnf -y install zip fail2ban screen make unzip git ca-certificates curl yum-utils device-mapper-persistent-data lvm2
-        sudo yum install -y python-pip
-        sudo pip install --upgrade pip
-        sudo pip install docker-compose
-        sudo yum upgrade python*
-        sudo systemctl start fail2ban
-        sudo systemctl enable fail2ban
-        echo ""
-        echo ""
-        echo "Permitting ports through the firewall."
-        sudo firewall-cmd --permanent --add-port=53595/tcp
-        sudo firewall-cmd --permanent --add-port=8080/tcp
-        sudo firewall-cmd --permanent --add-port=9000/tcp
-        sudo firewall-cmd --permanent --add-port=443/tcp
-        sudo firewall-cmd --permanent --add-port=22/tcp
-        sudo firewall-cmd --reload
-        echo ""
-        echo ""
-        echo "Do you have Oracle Java JDK 8 and Ant installed already? It is required for this."
-        echo ""
-        echo "${RED}1${NC} - Install for me!"
-        echo "${RED}2${NC} - Im all set"
-        echo ""
-        echo "Which of the above do you wish to do? Type the choice number and press enter."
-        echo ""
-        echo ""
-        read java
-
-        # CentOS 7 Java ===================================================>
-        if [ "$java" == "1" ]; then
-            sudo dnf -y install ant java-1.8.0-openjdk
-            wget --no-cookies \
-            --no-check-certificate \
-            --header "Cookie: oraclelicense=accept-securebackup-cookie" \
-            "http://download.oracle.com/otn-pub/java/jdk/8-b132/jdk-8-linux-x64.rpm" \
-            -O jdk-8-linux-x64.rpm
-            sudo rpm -Uvh jdk-8-linux-x64.rpm
-            sudo alternatives --install /usr/bin/java java /usr/java/jdk1.8.0/jre/bin/java 20000
-            sudo alternatives --install /usr/bin/jar jar /usr/java/jdk1.8.0/bin/jar 20000
-            sudo alternatives --install /usr/bin/javac javac /usr/java/jdk1.8.0/bin/javac 20000
-            sudo alternatives --install /usr/bin/javaws javaws /usr/java/jdk1.8.0/jre/bin/javaws 20000
-        else
-          continue
-        fi
-        # CentOS 7 Java <===================================================
-
-        echo ""
-        echo ""
-        echo "Do you have Docker installed? It is required for this."
-        echo ""
-        echo "${RED}1${NC} - No, install it for me!"
-        echo "${RED}2${NC} - Yes"
-        echo ""
-        echo "Which of the above do you wish to do? Type the choice number and press enter."
-        echo ""
-        echo ""
-        read docker
-
-        # CentOS 7 Docker ===================================================>
-        if [ "$docker" == "1" ]; then
-            echo "Removing any old versions of Docker that might confict."
-            echo ""
-            sudo dnf -y remove docker \
-                 docker-client \
-                 docker-client-latest \
-                 docker-common \
-                 docker-latest \
-                 docker-latest-logrotate \
-                 docker-logrotate \
-                 docker-selinux \
-                 docker-engine-selinux \
-                 docker-engine
-            echo ""
-            echo ""
-            echo "Attempting to install Docker now."
-            echo ""
-            curl -fsSL https://get.docker.com/ | sh
-            sudo systemctl start docker
-            sudo systemctl enable docker
-            sudo usermod -aG docker $USER
-            sudo dnf -y install docker-compose
-        else
-          continue
-        fi
-        # CentOS 7 Docker <===================================================
-    # CentOS 7 OS <===================================================
 
     # Mac OS ===================================================>
     elif [ "$os" == "4" ]; then
@@ -415,56 +212,6 @@ if [ "$install" == "1" ]; then
         # Mac Docker <===================================================
     # Mac OS <===================================================
 
-    # Other OS ===================================================>
-    elif [ "$os" == "5" ]; then
-        echo ""
-        echo "You will have to install Git manually then. Press enter to continue."
-        echo ""
-        read
-        echo ""
-        echo "Do you have Docker installed? It is required for this."
-        echo ""
-        echo "${RED}1${NC} - No, install it for me!"
-        echo "${RED}2${NC} - Yes"
-        echo ""
-        echo "Which of the above do you wish to do? Type the choice number and press enter."
-        echo ""
-        echo ""
-        read docker
-
-        # Other OS Docker ===================================================>
-        if [ "$docker" == "1" ]; then
-            echo "Attempting to install Docker now. If it fails, install manually."
-            echo ""
-            curl -fsSL get.docker.com -o get-docker.sh
-            sudo sh get-docker.sh
-        else
-          continue
-        fi
-        # Other OS Docker <===================================================
-
-        echo ""
-        echo ""
-        echo "Do you have Oracle Java JDK 8 and Ant installed already? It is required for this. If it fails, install manually."
-        echo ""
-        echo "${RED}1${NC} - Install for me!"
-        echo "${RED}2${NC} - Im all set"
-        echo ""
-        echo "Which of the above do you wish to do? Type the choice number and press enter."
-        echo ""
-        echo ""
-        read java
-
-        # Other OS Java ===================================================>
-        if [ "$java" == "1" ]; then
-            sudo apt-get remove openjdk-6-jre default-jre default-jre-headless -y
-            sudo add-apt-repository ppa:webupd8team/java -y && sudo apt update && sudo apt install ant oracle-java8-set-default openjfx -y
-        else
-          continue
-        fi
-        # Other OS Java <===================================================
-    # Other OS <===================================================
-
     else
       continue
     fi
@@ -489,7 +236,6 @@ Choices:
   ${RED}1${NC} - Set up for single player
   ${RED}2${NC} - Deployment for a publicly hosted server
   ${RED}3${NC} - Backup all SQL databases
-  ${RED}4${NC} - Restore all SQL databases
 "
 echo ""
 echo "Which of the above do you wish to do? Type the choice number and press enter."
@@ -611,21 +357,6 @@ elif [ "$choice" == "3" ]; then
   read
   ./Linux_Installer.sh
 # 3. Backup <===================================================
-
-# 4. Restore ===================================================>
-elif [ "$choice" == "4" ]; then
-
-  echo "You have picked ${GREEN}restore all SQL databases!${NC}"
-  echo ""
-  echo ""
-  sudo make restore
-  echo ""
-  echo ""
-  echo "Done! - Press enter to return back to the menu."
-  echo ""
-  read
-  ./Linux_Installer.sh
-# 4. Restore <===================================================
 
 else
     echo ""
