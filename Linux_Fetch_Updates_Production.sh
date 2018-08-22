3978d3271481d3d534c621349fb463645a5f97e8
Marwolf <cleako@gmail.com>
2018-08-22 12:05:39 -0400

    Verbose output sent to logs, permissions set

--- Linux_Fetch_Updates_Production.sh
+++ Linux_Fetch_Updates_Production.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
+exec 0</dev/tty
+
+# Open RSC: A replica RSC private server framework
+#
+# Installs and updates Open RSC
+#
+# Install with this command (from your Linux machine):
+#
+# curl -sSL https://raw.githubusercontent.com/Open-RSC/Docker-Home/master/Linux_Cloner.sh | bash
 
 source .env
+
+rm updater.log
+touch updater.log && chmod 777 updater.log | tee updater.log | &>/dev/null
 
 # Check for any updates to the game server
 clear
 echo "Pulling recent updates from the Open-RSC Game repository."
 cd Game
-sudo git pull &>/dev/null
+sudo git pull | tee -a ../updater.log &>/dev/null
 
 # Verifies permissions are set correctly
-sudo chmod -R 777 Game &>/dev/null
-sudo setfacl -m user:$USER:rw /var/run/docker.sock &>/dev/null
+sudo chmod -R 777 Game | tee -a ../updater.log &>/dev/null
+sudo setfacl -m user:$USER:rw /var/run/docker.sock | tee -a ../updater.log &>/dev/null
 cd ..
 
 clear
@@ -28,11 +40,10 @@ sudo nano .env
 sudo nano Game/client/src/org/openrsc/client/Config.java
 sudo nano Game/Launcher/src/Main.java
 sudo nano Game/server/config/config.xml
-sudo nano etc/ghost/config.production.json
 
 clear
 echo "Restarting Ghost container"
-sudo docker stop ghost && sudo docker start ghost &>/dev/null
+sudo docker stop ghost && sudo docker start ghost | tee -a updater.log &>/dev/null
 fi
 
 if [ "$edit" == "2" ]; then
@@ -42,34 +53,33 @@ fi
 # Server
 cd Game
 clear
-echo "Compiling the game server. Any errors will be in compile.log"
-touch ../compile.log && chmod 777 ../compile.log &>/dev/null
-sudo ant -f "server/build.xml" compile | tee ../compile.log &>/dev/null
+echo "Compiling the game server. Any errors will be in updater.log"
+sudo ant -f "server/build.xml" compile | tee ../updater.log &>/dev/null
 
 # Client
 clear
-echo "Compiling and preparing the game client. Any errors will be in compile.log"
-sudo ant -f "client/build.xml" compile | tee -a ../compile.log &>/dev/null
+echo "Compiling and preparing the game client. Any errors will be in updater.log"
+sudo ant -f "client/build.xml" compile | tee -a ../updater.log &>/dev/null
 cd client
-sudo zip -r "client.zip" "Open_RSC_Client.jar" &>/dev/null
+sudo zip -r "client.zip" "Open_RSC_Client.jar" | tee -a ../../updater.log &>/dev/null
 cd ../
-yes | sudo cp -rf "client/client.zip" "../Website/downloads/" &>/dev/null
-sudo rm "client/client.zip" &>/dev/null
+yes | sudo cp -rf "client/client.zip" "../Website/downloads/" | tee -a ../updater.log &>/dev/null
+sudo rm "client/client.zip" | tee -a ../updater.log &>/dev/null
 
 # Launcher
 clear
-echo "Compiling and preparing the game launcher. Any errors will be in compile.log"
-sudo ant -f "Launcher/build.xml" jar | tee -a ../compile.log &>/dev/null
-yes | sudo cp -rf "Launcher/dist/Open_RSC_Launcher.jar" "../Website/downloads/" &>/dev/null
+echo "Compiling and preparing the game launcher. Any errors will be in updater.log"
+sudo ant -f "Launcher/build.xml" jar | tee -a ../updater.log &>/dev/null
+yes | sudo cp -rf "Launcher/dist/Open_RSC_Launcher.jar" "../Website/downloads/" | tee -a updater.log &>/dev/null
 
 # Cache
 clear
 echo "Preparing the cache."
-yes | sudo cp -rf "client/cache.zip" "../Website/downloads/" &>/dev/null
+yes | sudo cp -rf "client/cache.zip" "../Website/downloads/" | tee -a ../updater.log &>/dev/null
 cd ..
-sudo rm Website/downloads/hashes.txt &>/dev/null
-md5sum Website/downloads/client.zip | grep ^[a-zA-Z0-9]* | awk '{print "client="$1}' | sudo tee -a Website/downloads/hashes.txt &>/dev/null
-md5sum Website/downloads/cache.zip | grep ^[a-zA-Z0-9]* | awk '{print "cache="$1}' | sudo tee -a Website/downloads/hashes.txt &>/dev/null
+sudo rm Website/downloads/hashes.txt | tee -a updater.log &>/dev/null
+md5sum Website/downloads/client.zip | grep ^[a-zA-Z0-9]* | awk '{print "client="$1}' | sudo tee -a Website/downloads/hashes.txt | tee -a updater.log &>/dev/null
+md5sum Website/downloads/cache.zip | grep ^[a-zA-Z0-9]* | awk '{print "cache="$1}' | sudo tee -a Website/downloads/hashes.txt | tee -a updater.log &>/dev/null
 
 # Database
 clear
@@ -77,4 +87,4 @@ echo "Preparing the database."
 docker exec -i $(sudo docker-compose ps -q mysqldb) mysql -u"$MYSQL_ROOT_USER" -p"$MYSQL_ROOT_PASSWORD" < Game/Databases/openrsc_config.sql 2>/dev/null
 
 # Run the game server in a detached screen
-sudo ./Linux_Run_Production_Server.sh
+./Linux_Run_Production_Server.sh
--- Linux_Single_Player.sh
+++ Linux_Single_Player.sh
@@ -1,33 +1,45 @@
 #!/bin/bash
+exec 0</dev/tty
+
+# Open RSC: A replica RSC private server framework
+#
+# Installs and updates Open RSC
+#
+# Install with this command (from your Linux machine):
+#
+# curl -sSL https://raw.githubusercontent.com/Open-RSC/Docker-Home/master/Linux_Cloner.sh | bash
 
 source .env
+
+rm singleplayer.log
+touch singleplayer.log && chmod 777 singleplayer.log | tee -a singleplayer.log &>/dev/null
 
 # Check for any updates to the game server
 clear
 echo "Pulling recent updates from the Open-RSC Game repository."
-sudo git pull &>/dev/null
-sudo make pull-game &>/dev/null
+sudo git pull | tee -a singleplayer.log &>/dev/null
+sudo make pull-game | tee -a singleplayer.log &>/dev/null
+sudo chmod -R 777 Game | tee -a installer.log &>/dev/null
 
 clear
 echo "Starting Docker containers."
-sudo make stop &>/dev/null
-sudo make start-single-player &>/dev/null
+sudo make stop | tee -a singleplayer.log &>/dev/null
+sudo make start-single-player | tee -a singleplayer.log &>/dev/null
 
 # Compile the game server and client
 clear
-echo "Compiling the game client. Any errors will be in compile.log"
-touch compile.log && chmod 777 compile.log &>/dev/null
-sudo ant -f "Game/client/build.xml" compile | tee compile.log &>/dev/null
+echo "Compiling the game client. Any errors will be in singleplayer.log"
+sudo ant -f "Game/client/build.xml" compile | tee -a singleplayer.log &>/dev/null
 
 clear
-echo "Compiling the game server. Any errors will be in compile.log"
-sudo ant -f "Game/server/build.xml" compile | tee -a compile.log &>/dev/null
+echo "Compiling the game server. Any errors will be in singleplayer.log"
+sudo ant -f "Game/server/build.xml" compile | tee -a singleplayer.log &>/dev/null
 
 #Create game cache
 clear
 echo "Removing old cache if exists and then extracting a fresh client cache to your home folder."
-sudo rm -rf ~/OpenRSC &>/dev/null
-mkdir ~/OpenRSC &>/dev/null
+sudo rm -rf ~/OpenRSC | tee -a singleplayer.log &>/dev/null
+mkdir ~/OpenRSC | tee -a singleplayer.log &>/dev/null
 unzip -o Game/client/cache.zip -d ~/OpenRSC >/dev/null
 
 #Import fresh version of config database
@@ -38,10 +50,10 @@ sudo docker exec -i $(sudo docker-compose ps -q mysqldb) mysql -u"$MYSQL_ROOT_US
 #Generate updated cache files, copies them to cache folder overwriting existing
 clear
 echo "Generating cache .dat files from current config database and copying to client cache in your home folder."
-sudo ant -f Game/server/build.xml npcs items objects &>/dev/null
-yes | cp -rf Game/server/npcs.dat ~/OpenRSC/npcs.dat &>/dev/null
-yes | cp -rf Game/server/objects.dat ~/OpenRSC/objects.dat &>/dev/null
-yes | cp -rf Game/server/items.dat ~/OpenRSC/items.dat &>/dev/null
+sudo ant -f Game/server/build.xml npcs items objects | &>/dev/null
+yes | cp -rf Game/server/npcs.dat ~/OpenRSC/npcs.dat | tee -a singleplayer.log &>/dev/null
+yes | cp -rf Game/server/objects.dat ~/OpenRSC/objects.dat | tee -a singleplayer.log &>/dev/null
+yes | cp -rf Game/server/items.dat ~/OpenRSC/items.dat | tee -a singleplayer.log &>/dev/null
 
 # Run the game client in a new window
 clear
