e7a40ee00699127d1fff5b9307a3eea02365d1b4
Marwolf <cleako@gmail.com>
2018-08-22 11:09:24 -0400

    Fix

--- Linux_Cloner.sh
+++ Linux_Cloner.sh
@@ -8,8 +8,6 @@
 #
 # curl -sSL https://raw.githubusercontent.com/Open-RSC/Docker-Home/master/Linux_Cloner.sh | bash
 
-choice=""
-
 cd /
 sudo git clone https://github.com/Open-RSC/Docker-Home.git &>/dev/null
 cd Docker-Home
